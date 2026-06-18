d68ba150325f3e984354e5b494ec6cb345faa4e3
mahesheyellu <maheshyellu99@gmail.com>
2026-06-18 09:59:44 +0530

    Added Streaming and Batch

--- updatedlangchain/2-modelintegration.py
+++ updatedlangchain/2-modelintegration.py
@@ -37,3 +37,21 @@ from langchain_groq import ChatGroq
 model = ChatGroq( model = "qwen/qwen3-32b" )
 response = model.invoke( "Why do parrots talk?" )
 print( response.content )
+
+
+## Streaming
+# Most Models Can stream their output content while it is being generated. By displaying output progressively, streaming significantly improves user experience, particularly for longer responses. Calling stream() return as iterator that yields output chunks as they are produced. you can use a loop to process each chunk in real-time:
+for chunk in model.stream( "Write me a 200 words paragraph on artificial Intelligence" ):
+    print( chunk.text, end= "|", flush=True )
+
+
+# Batch
+# Batching a collection of independent requests to a model can siginficantly improve performance and reduce costs, as the processing can be done in parallel:
+
+responses = model.batch([
+    "Why do parrots have colorful feathers?",
+    "How do airplanes fly?",
+    "What is quantum computing?"
+])
+for response in responses:
+    print(response)
